afb1b277a588ebadba2d5c657f535cdae53fca77
Olha Zarichna <olya.zarichna@gmail.com>
2023-04-29 00:57:38 +0300

    fixed style, add hover, focus

--- src/components/TeamSection/TeamSection.jsx
+++ src/components/TeamSection/TeamSection.jsx
@@ -13,6 +13,7 @@ import {
   person3Webp1x,
   person3Webp2x,
 } from "./gallery";
+import OverlayElement from "../OverlayElement/OverlayElement";
 
 const TeamSection = () => {
   return (
@@ -28,59 +29,78 @@ const TeamSection = () => {
 
       <ul className={styles.list}>
         <li className={styles.listItem}>
-          <picture>
-            <source
-              srcSet={`${person1Webp1x} 1x, ${person1Webp2x} 2x`}
-              type="image/webp"
-            />
-            <source
-              srcSet={`${person1_1x} 1x, ${person1_2x} 2x`}
-              type="image/jpeg"
-            />
-            <img
-              className={styles.img}
-              src={person1_1x}
-              alt="John Doe, president of company"
-            />
-          </picture>
+          <div className={styles.overlay}>
+            <picture>
+              <source
+                srcSet={`${person1Webp1x} 1x, ${person1Webp2x} 2x`}
+                type="image/webp"
+              />
+              <source
+                srcSet={`${person1_1x} 1x, ${person1_2x} 2x`}
+                type="image/jpeg"
+              />
+              <img
+                className={styles.img}
+                src={person1_1x}
+                alt="John Doe, president of company"
+              />
+            </picture>
+
+            <div className={styles.overlayElement}>
+              <OverlayElement />
+            </div>
+          </div>
+
           <h3 className={styles.caption}>John Doe</h3>
           <p className={styles.title}>President</p>
         </li>
         <li className={styles.listItem}>
-          <picture>
-            <source
-              srcSet={`${person2Webp1x} 1x, ${person2Webp2x} 2x`}
-              type="image/webp"
-            />
-            <source
-              srcSet={`${person2_1x} 1x, ${person2_2x} 2x`}
-              type="image/jpeg"
-            />
-            <img
-              className={styles.img}
-              src={person2_1x}
-              alt="Jane Doe, Vice President of company"
-            />
-          </picture>
+          <div className={styles.overlay}>
+            <picture>
+              <source
+                srcSet={`${person2Webp1x} 1x, ${person2Webp2x} 2x`}
+                type="image/webp"
+              />
+              <source
+                srcSet={`${person2_1x} 1x, ${person2_2x} 2x`}
+                type="image/jpeg"
+              />
+              <img
+                className={styles.img}
+                src={person2_1x}
+                alt="Jane Doe, Vice President of company"
+              />
+            </picture>
+            <div className={styles.overlayElement}>
+              <OverlayElement />
+            </div>
+          </div>
           <h3 className={styles.caption}>Jane Doe</h3>
           <p className={styles.title}>Vice President</p>
         </li>
         <li className={styles.listItem}>
-          <picture>
-            <source
-              srcSet={`${person3Webp1x} 1x, ${person3Webp2x} 2x`}
-              type="image/webp"
-            />
-            <source
-              srcSet={`${person3_1x} 1x, ${person3_2x} 2x`}
-              type="image/jpeg"
-            />
-            <img
-              className={styles.img}
-              src={person3_1x}
-              alt="Steve Smith, Marketing Head"
-            />
-          </picture>
+          <div className={styles.overlay}>
+            <picture>
+              <source
+                srcSet={`${person3Webp1x} 1x, ${person3Webp2x} 2x`}
+                type="image/webp"
+              />
+              <source
+                srcSet={`${person3_1x} 1x, ${person3_2x} 2x`}
+                type="image/jpeg"
+              />
+              <img
+                className={styles.img}
+                src={person3_1x}
+                alt="Steve Smith, Marketing Head"
+              />
+            </picture>
+
+            <div className={styles.overlayElement}>
+              <OverlayElement />
+            </div>
+          </div>
+
           <h3 className={styles.caption}>Steve Smith</h3>
           <p className={styles.title}>Marketing Head</p>
         </li>
